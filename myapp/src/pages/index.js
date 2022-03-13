981696e72ac7b64aeb24d9dc9633f991fc0b2046
Anaple <67675386+Anaple@users.noreply.github.com>
2022-03-13 18:18:42 +0800

    plyloader.js 添加

--- myapp/src/pages/index.js
+++ myapp/src/pages/index.js
@@ -1,9 +1,10 @@
 import styles from './index.css';
 import 'antd/dist/antd.css';
 import {useState,useEffect} from 'react';
-import { Button, Radio,message,Progress,Card } from 'antd';
-import { Input, Space } from 'antd';
+import { Button,message,Progress,Card } from 'antd';
+import { Input } from 'antd';
 import * as THREE from 'three'
+import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js';
 import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 let camera, scene, renderer;
@@ -27,11 +28,12 @@ function render() {
 
 
 
+
 const { Search } = Input;
 
 export default function() {
   useEffect(() => {
-    init("http://192.168.0.204:8080/files/rgb.pcd");
+    init("http://127.0.0.1:8080/files/rgb.ply");
     render();
   }, []);
 
@@ -45,7 +47,7 @@ export default function() {
 
 
   const sysCard = () => {
-    console.log(cardShow)
+   
     if(cardShow){
       return(
       <Card title="状态" bordered={false} extra={<a onClick={()=>setCardShow(false)}>x</a>} style={{ width: 300 }}>
@@ -74,7 +76,6 @@ export default function() {
     }
     
   }
-
   function init(openurl) {
     var container = document.getElementById("qt");
     
@@ -100,10 +101,37 @@ export default function() {
     //controls.minPolarAngle = Math.PI;
     controls.minAzimuthAngle = 2*Math.PI
   
-    const loader = new PCDLoader();
+   
     var url = openurl
+    var file_type = url.substr(url.length -3 ,url.length)
+    console.log(file_type)
+    
   try{
-    loader.load(url, function (points) {
+    if(file_type === "ply"){
+    const loader1 = new PLYLoader()
+    loader1.load(
+      url,
+      function (geometry) {
+          // geometry.computeVertexNormals()
+          // const material = new THREE.MeshStandardMaterial( { color: 0x0055ff, flatShading: true } );
+          const mesh = new THREE.MeshBasicMaterial(geometry)
+         
+          mesh.rotateX(-Math.PI / 2)
+          mesh.scale.multiplyScalar( 0.01 );
+          // mesh.geometry.computeBoundingBox()
+          scene.add(mesh)
+          render();
+      },
+      (xhr) => {
+        setProgcress(Math.floor(xhr.loaded/xhr.total*100))
+      },
+      (error) => {
+          console.log(error)
+      }
+  )
+    }if(file_type ==="pcd"){
+    const loader2 = new PCDLoader();
+    loader2.load(url, function (points) {
       points.geometry.center();
       points.geometry.rotateX(Math.PI);
       points.scale.multiplyScalar( 0.3 );
@@ -113,6 +141,10 @@ export default function() {
     },(xhr)=>{
       setProgcress(Math.floor(xhr.loaded/xhr.total*100))
     });
+  }else{
+    
+   
+  }
   }catch(err){
     message.error({content:err,duration:1})
   }
@@ -121,6 +153,71 @@ export default function() {
   
   
   }
+
+//   function init(openurl) {
+//     var container = document.getElementById("qt");
+    
+//     renderer = new THREE.WebGLRenderer({ antialias: true });
+//     renderer.setPixelRatio(window.devicePixelRatio);
+//     renderer.setSize(window.innerWidth , window.innerHeight );
+//     container.appendChild(renderer.domElement);
+    
+//     scene = new THREE.Scene();
+  
+//     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+  
+//     camera.position.set( 0, 0, 600 );
+//     camera.up.set(0,1,0)
+  
+//     scene.add( camera );
+  
+//     const controls = new OrbitControls( camera, renderer.domElement );
+//     controls.addEventListener( 'change', render ); // use if there is no animation loop
+    
+//     controls.minDistance = 0.5;
+//     controls.maxDistance =100;
+//     //controls.minPolarAngle = Math.PI;
+//     controls.minAzimuthAngle = 2*Math.PI
+  
+//   //   const loader = new PCDLoader();
+//   //   var url = openurl
+//   // try{
+//   //   loader.load(url, function (points) {
+//   //     points.geometry.center();
+//   //     points.geometry.rotateX(Math.PI);
+//   //     points.scale.multiplyScalar( 0.3 );
+//   //     scene.add(points);
+//   //     render();
+  
+//   //   },(xhr)=>{
+//   //    
+//   //   });
+//   // }catch(err){
+//   //   message.error({content:err,duration:1})
+//   // }
+  
+//   //   window.addEventListener('resize', onWindowResize);
+  
+  
+//   // }
+//   const loader = new PLYLoader()
+//   loader.load(
+//     openurl,
+//     function (geometry) {
+//         geometry.computeVertexNormals()
+//         const mesh = new THREE.Mesh(geometry)
+//         mesh.scale.multiplyScalar( 0.006 );
+//         scene.add(mesh)
+//         render();
+//     },
+//     (xhr) => {
+//       setProgcress(Math.floor(xhr.loaded/xhr.total*100))
+//     },
+//     (error) => {
+//         console.log(error)
+//     }
+// )
+//   }
 
   return (
     <div id='qt' >
--- webcilent/src/pages/SharePage/index.jsx
+++ webcilent/src/pages/SharePage/index.jsx
@@ -111,22 +111,46 @@ export default function () {
     //controls.minPolarAngle = Math.PI;
     controls.minAzimuthAngle = 2 * Math.PI;
 
-    const loader = new PCDLoader();
     var url = openurl;
+    var file_type = url.substr(url.length - 3, url.length);
     try {
-      loader.load(
-        url,
-        function (points) {
-          points.geometry.center();
-          points.geometry.rotateX(Math.PI);
-          points.scale.multiplyScalar(0.3);
-          scene.add(points);
-          render();
-        },
-        (xhr) => {
-          setProgcress(Math.floor((xhr.loaded / xhr.total) * 100));
-        },
-      );
+      if (file_type === 'ply') {
+        const loader1 = new PLYLoader();
+        loader1.load(
+          url,
+          function (geometry) {
+            geometry.computeVertexNormals();
+            const mesh = new THREE.Mesh(geometry);
+            mesh.scale.multiplyScalar(0.006);
+            scene.add(mesh);
+            render();
+          },
+          (xhr) => {
+            setProgcress(Math.floor((xhr.loaded / xhr.total) * 100));
+          },
+          (error) => {
+            console.log(error);
+          },
+        );
+      }
+      if (file_type === 'pcd') {
+        const loader2 = new PCDLoader();
+        loader2.load(
+          url,
+          function (points) {
+            points.geometry.center();
+            points.geometry.rotateX(Math.PI);
+            points.scale.multiplyScalar(0.3);
+            scene.add(points);
+            render();
+          },
+          (xhr) => {
+            setProgcress(Math.floor((xhr.loaded / xhr.total) * 100));
+          },
+        );
+      } else {
+        message.error({ content: '不支持的文件类型', duration: 1 });
+      }
     } catch (err) {
       message.error({ content: err, duration: 1 });
     }
